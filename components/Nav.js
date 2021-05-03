b11667b3bb96a88cc47c6575f34a3dfd77bf1e5e
Jeren <jobuilds@outlook.com>
2021-05-03 13:44:26 -0400

    completed the header and started work on building out the api call for the nav bar

---
+++ components/Nav.js
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+        <nav>
+            <div>
+                {Object.entries(requests).map([key, { title, url }])}
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
-
-      {/* Nav */}
+      <Nav />
 
       {/* Results */}
     </div>
